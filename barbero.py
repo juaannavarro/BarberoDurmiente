0bff0e901dc61c62a9ea92a3cc09840844feabeb
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-05-18 12:12:44 +0200

    cambios

--- barbero.py
+++ barbero.py
@@ -5,40 +5,79 @@ class Peluqueria:
     def __init__(self):
         self.peluquero = threading.Semaphore(0)
         self.cliente = threading.Semaphore(0)
-        self.silla = threading.Semaphore(4)
+        self.silla = threading.Semaphore(3)
         self.control = threading.Semaphore(1)
         
     def cortarPelo(self):
-        print("Peluquero cortando pelo")
-        time.sleep(1)
-        print("Peluquero termino de cortar pelo")
+        print("Peluquero cortando el pelo")
+        time.sleep(3)
+        print("Peluquero termina de cortar el pelo")
         
     def Cliente(self):
-        print("Cliente ha llegado")
+        print("Cliente llega a la peluqueria")
         self.control.acquire()
         
-        if self.silla._value > 0:
+        if self.silla._value > 0 and self.silla._value <= 3:
+            print('------Hay sillas disponibles------')
             self.silla.acquire()
             print("Cliente se sienta en la silla de espera")
+            time.sleep(3)
             self.control.release()
-            self.cliente.release()
-            print("Cliente despertando al peluquero")    
-            self.peluquero.acquire()
-            self.silla.release()
-            print("Cliente deja la silla de espera")
-            self.cortarPelo()
-        else:
+            if self.peluquero._value == 0:
+                self.cliente.release()
+                print("Cliente despierta al peluquero")
+                time.sleep(3)
+                self.peluquero.acquire()
+                self.silla.release()
+                print("Cliente deja la silla de espera")
+                time.sleep(3)
+                print("Cliente se sienta en la silla del peluquero")
+                self.cortarPelo()
+            else:
+                time.sleep(3)  
+                self.peluquero.acquire()
+                self.silla.release()
+                print("Cliente deja la silla de espera")
+                time.sleep(3)
+                self.cortarPelo()
+        elif self.silla._value >= 4:
             self.control.release()
             print("No hay sillas disponibles")
-        time.sleep(4)
+        time.sleep(3)
             
     def Peluquero(self):
+        if self.cliente._value == 0:
+            print("Peluquero durmiendo", end="\n\n")
         while True:
             self.cliente.acquire()
             self.peluquero.release()
-            time.sleep(4)
+            self.cortarPelo()
+            self.silla.release()
+            time.sleep(3)
             
+            
+def main():
+    peluqueria = Peluqueria()
+    
+    peluquero_thread = threading.Thread(target=peluqueria.Peluquero)
+    peluquero_thread.start()
+    
+    clientes_threads = []
+    for i in range(10):
+        cliente_thread = threading.Thread(target=peluqueria.Cliente)
+        clientes_threads.append(cliente_thread)
+        cliente_thread.start()  
+        
+        time.sleep(3)
+        
+    for cliente_thread in clientes_threads:
+        cliente_thread.join()   
+        
+    peluquero_thread.join()
             
 
+if __name__ == "__main__":
+    main()    
     
-    
+
+
--- main.py
+++ main.py
@@ -6,11 +6,12 @@ def main():
     peluquero_thread.start()
         
     clientes_threads = []
+    print("Creando clientes", end="\n\n")
     for i in range(10):
         cliente_thread = threading.Thread(target=peluqueria.Cliente)
         clientes_threads.append(cliente_thread)
         cliente_thread.start()  
-        time.sleep(1)
+        time.sleep(3)
         
     for cliente_thread in clientes_threads:
         cliente_thread.join()   
